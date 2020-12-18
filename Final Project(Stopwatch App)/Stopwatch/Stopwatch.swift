fd3ba218fdcdbc3e0e7cabff2c257940c6ec3517
xz4999 <xz4999@users.noreply.github.com>
2020-12-17 17:03:08 -0800

    Final Project

---
+++ Final Project(Stopwatch App)/Stopwatch/Stopwatch.swift
@@ -0,0 +1,17 @@
+//
+//  Stopwatch.swift
+//  Stopwatch
+//
+//
+
+import Foundation
+
+class Stopwatch: NSObject {
+  var counter: Double
+  var timer: Timer
+  
+  override init() {
+    counter = 0.0
+    timer = Timer()
+  }
+}
